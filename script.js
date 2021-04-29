1eb5500cdc7c58d424757d3233f34b31111cb2a8
ycontreras98 <y.contreras@duocuc.cl>
2021-04-29 17:32:20 -0400

    cambios 29-04

--- script.js
+++ script.js
@@ -6,26 +6,21 @@ $(document).ready(function() {
     $("#formulario").submit(function() {
         var mensaje = "";
         if ($("#nombre").val().trim().length == 0) {
-            mensaje = "El campo nombre está en blanco";
+            mensaje = "El campo nombre está en blanco!";
         }
         if ($("#correo").val().trim().length == 0) {
-            mensaje = "El campo email está en blanco";
+            mensaje = "El campo email está en blanco!";
         }
         if ($("#correo").val().trim().length == 0) {
-            mensaje = "El campo email está en blanco";
+            mensaje = "El campo email está en blanco!";
         }
         if ($("#celular").val().trim().length == 0) {
-            mensaje = "El campo celular está en blanco";
+            mensaje = "El campo celular está en blanco!";
         }
         if (mensaje != "") {
             $("#error").html(mensaje);
             $("#error").show();
             event.preventDefault();
         }
-    });
-    $("#enviar").click(function() {
-        $("#error").animate({
-            height: 'toggle'
-        });
     });
 });
