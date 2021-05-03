155620d23dc7462c255a6f716648d7cb886f6447
cristsanchezb <cris.sanchezb@duocuc.cl>
2021-05-02 21:49:54 -0400

    avance

---
+++ script.js
@@ -0,0 +1,86 @@
+indow.onload = iniciar;
+
+function iniciar() {
+    document.getElementById("enviar").addEventListener('click', validar, false);
+}
+
+function validaNombre() {
+    var elemento = document.getElementById("nombre");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error2(elemento, "Debe introducir un nombre")
+        }
+        if (elemento.validity.patternMismatch) {
+            error2(elemento, "El nombre debe tener entre 2 y 15 caracteres");
+        }
+        //error(elemento);
+        return false;
+    }
+    return true;
+}
+
+function validaEdad() {
+    var elemento = document.getElementById("edad");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error2(elemento, "Debe introducir una edad")
+        }
+        if (elemento.validity.rangeOverflow) {
+            error2(elemento, "La edad debe ser menor a 100")
+        }
+        if (elemento.validity.rangeUnderflow) {
+            error2(elemento, "Debe ser mayor de 15 años para enviar un formulario");
+        }
+        //error(elemento);
+        return false;
+    }
+    return true;
+}
+
+
+
+function validaCelular() {
+    var elemento = document.getElementById("celular");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error2(elemento, "Debe introducir un celular")
+        }
+        if (elemento.validity.patternMismatch) {
+            error2(elemento, "El celular debe tener 9 numeros");
+        } //error(elemento);
+        return false;
+    }
+    return true;
+}
+
+
+
+
+function validar(e) {
+    borrarError();
+    if (validaNombre() && validaEdad() && validaCelular() && confirm("Pulsa aceptar si deseas enviar el formulario")) {
+        return true
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
+
+function error(elemento) {
+    document.getElementById("mensajeError").innerHTML = elemento.validationMessage;
+    elemento.className = "error";
+    elemento.focus();
+}
+
+function error2(elemento, mensaje) {
+    document.getElementById("mensajeError").innerHTML = mensaje;
+    elemento.className = "error";
+    elemento.focus();
+}
+
+function borrarError() {
+    var formulario = document.forms[0];
+    for (var i = 0; i < formulario.elements.length; i++) {
+        formulario.elements[i].className = "";
+    }
+}
